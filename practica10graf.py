d445ac0a81f98e82ee21b21a4bfd0dacba0aae3d
dani1563 <53991380+dani1563@users.noreply.github.com>
2020-06-07 21:31:45 -0500

    Update practica10graf.py

--- practica10graf.py
+++ practica10graf.py
@@ -1,4 +1,19 @@
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from numpy import *
 
-x = lins 
+x = linspace(0, 5, 20)
+
+fig, ax = plt.subplots(facecolor='w', edgecolor='k')
+ax.plot(x, sin(x), marker="o", color="r", linestyle='None')
+
+ax.grid(True)
+ax.set_xlabel('X') #Eje x
+ax.set_ylabel('Y') #Eje y
+ax.grid(True)
+ax.legend(["y = x**2"])
+
+plt.title('Puntos')
+plt.show()
+
+fig.savefig("grafica.png") #Guardando 
